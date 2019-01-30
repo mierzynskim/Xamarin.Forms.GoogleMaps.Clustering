a5c3ab91ce3932c5048226d907ce70715a3f5764
Michał Mierzyński <mierzynski.michal@gmail.com>
2019-01-30 12:06:12 +0100

    Added sample project

---
+++ Sample/XFGoogleMapSample/ClusterPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.GoogleMaps;
+
+namespace XFGoogleMapSample
+{
+    public partial class ClusterPage
+    {
+        private const int ClusterItemsCount = 1000;// 10000;
+        private const double Extent = 0.2;
+        private Position _currentPosition = new Position(31.768319, 35.213710);
+        private readonly Random _random = new Random();
+
+        public ClusterPage()
+        {
+            InitializeComponent();
+
+            this.Map.MoveToRegion(MapSpan.FromCenterAndRadius(this._currentPosition, Distance.FromMeters(100)));
+            
+            for (var i = 0; i <= ClusterItemsCount; i++)
+            {
+                var lat = this._currentPosition.Latitude + Extent * GetRandomNumber(-1.0, 1.0);
+                var lng = this._currentPosition.Longitude + Extent * GetRandomNumber(-1.0, 1.0);
+
+                this.Map.ClusteredPins.Add(new Pin()
+                {
+                    Position = new Position(lat, lng),
+                    Label = $"Item {i}",
+                    Icon = BitmapDescriptorFactory.FromBundle("image01.png")
+                });
+            }
+
+            Map.PinClicked += MapOnPinClicked;
+            //Map.CameraIdled += MapOnCameraIdled;
+            Map.InfoWindowClicked += MapOnInfoWindowClicked;
+            Map.InfoWindowLongClicked += MapOnInfoWindowLongClicked;
+
+            this.Map.Cluster();
+        }
+
+        private async void MapOnPinClicked(object sender, PinClickedEventArgs e)
+        {
+            await DisplayAlert("Pin clicked", e.Pin?.Label, "Cancel");
+        }
+
+        private async void MapOnCameraIdled(object sender, CameraIdledEventArgs e)
+        {
+            await DisplayAlert("Camera idled", $"{e.Position.Target.Latitude} {e.Position.Target.Longitude}", "Cancel");
+        }
+        
+        private async void MapOnInfoWindowClicked(object sender, InfoWindowClickedEventArgs e)
+        {
+            await DisplayAlert("Info clicked", $"{e.Pin?.Position.Latitude} {e.Pin?.Position.Longitude}", "Cancel");
+        }
+
+        private async void MapOnInfoWindowLongClicked(object sender, InfoWindowLongClickedEventArgs e)
+        {
+            await DisplayAlert("Info long clicked", $"{e.Pin?.Position.Latitude} {e.Pin?.Position.Longitude}", "Cancel");
+        }
+
+        private double GetRandomNumber(double minimum, double maximum)
+        {
+            return _random.NextDouble() * (maximum - minimum) + minimum;
+        }
+    }
+}
---
+++ Sample/iOS/Main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.GoogleMaps.Clustering;
+using Xamarin.Forms.GoogleMaps.Clustering.iOS;
+
+[assembly: ExportRenderer(typeof(ClusteredMap), typeof(ClusteredMapRenderer))]
+namespace XFGoogleMapSample.iOS
+{
+    public class Application
+    {
+        // This is the main entry point of the application.
+        static void Main(string[] args)
+        {
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+    }
+}
+
