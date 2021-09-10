4bef02b9a2cdf7e213d8da4dc0b2ae59f21e8ddd
Damian Dekoszczak <damian.dekoszczak@bsinternational.pl>
2021-09-10 13:49:35 +0200

    added xamarin.forms.v5 sample

---
+++ Sample.Forms.v5/Android/CachingNativeBitmapDescriptorFactory.cs
@@ -0,0 +1,49 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using Xamarin.Forms.GoogleMaps.Android.Factories;
+using Xamarin.Forms.GoogleMaps;
+using AndroidBitmapDescriptor = Android.Gms.Maps.Model.BitmapDescriptor;
+
+namespace Xamarin.Forms.v5.Sample.Droid
+{
+    public sealed class CachingNativeBitmapDescriptorFactory : IBitmapDescriptorFactory
+    {
+        private readonly ConcurrentDictionary<string, AndroidBitmapDescriptor> _cache
+            = new ConcurrentDictionary<string, AndroidBitmapDescriptor>();
+
+        public AndroidBitmapDescriptor ToNative(BitmapDescriptor descriptor)
+        {
+            var defaultFactory = DefaultBitmapDescriptorFactory.Instance;
+
+            if (!string.IsNullOrEmpty(descriptor.Id))
+            {
+                var cacheEntry = _cache.GetOrAdd(descriptor.Id, _ => defaultFactory.ToNative(descriptor));
+                return cacheEntry;
+            }
+
+            return defaultFactory.ToNative(descriptor);
+        }
+    }
+
+    public sealed class BitmapConfig : IBitmapDescriptorFactory
+    {
+        public AndroidBitmapDescriptor ToNative(BitmapDescriptor descriptor)
+        {
+            int iconId = 0;
+            switch (descriptor.Id)
+            {
+                case "icon":
+                    iconId = Resource.Drawable.icon;
+                    break;
+            }
+            return AndroidBitmapDescriptorFactory.FromResource(iconId);
+        }
+    }
+}
---
+++ Sample.Forms.v5/Sample/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.v5.Sample.ViewModels;
+
+namespace Xamarin.Forms.v5.Sample.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
